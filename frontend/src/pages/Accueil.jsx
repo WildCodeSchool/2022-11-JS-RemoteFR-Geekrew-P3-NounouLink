9c81b197b7a92d595a871639e508c8d76de2d5e9
Pierre Saint Aubin <p.saintaubin@hotmail.fr>
2023-02-23 17:58:37 +0100

    add navigate

--- frontend/src/pages/Accueil.jsx
+++ frontend/src/pages/Accueil.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import logo from "../assets/logo.svg";
 import hero from "../assets/hero.svg";
 
+const navigate = useNavigate();
+
 function Accueil() {
-  const handleClick = () => {};
+  const handleConnect = () => {
+    navigate("/connexion");
+  };
+  const handleCreate = () => {
+    navigate("/creation-compte");
+  };
+
   return (
     <div className="flex flex-col justify-evenly items-center gradient-linear h-full">
       <img className="scale-105" src={hero} alt="logo" />
@@ -13,10 +22,10 @@ function Accueil() {
         Garde d'enfant à la demande
       </h1>
 
-      <button type="button" className="btn-purple" onClick={handleClick}>
+      <button type="button" className="btn-purple" onClick={handleConnect}>
         Me connecter
       </button>
-      <button type="button" className="btn-orange ">
+      <button type="button" className="btn-orange " onClick={handleCreate}>
         Créer mon compte
       </button>
     </div>
