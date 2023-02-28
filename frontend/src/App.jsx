7d5e4203c21eb7c31194b075710a59ea151ff36e
irisge <[iris.genevrier@gmail.com](mailto:iris.genevrier@gmail.com)>
2023-02-28 15:39:36 +0100

    front design for search

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
 import Accueil from "./pages/Accueil";
 import Connexion from "./pages/Connexion";
+import Search from "./pages/Search";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route>
-          <Route path="/" element={<Home />} />
           <Route path="/accueil" element={<Accueil />} />
           <Route path="/connexion" element={<Connexion />} />
+          <Route path="/recherche" element={<Search />} />
         </Route>
       </Routes>
     </BrowserRouter>
