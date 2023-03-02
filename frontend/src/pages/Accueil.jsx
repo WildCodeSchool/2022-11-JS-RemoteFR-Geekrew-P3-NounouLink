b50678643601358c710b60b2142a3d25ad6dc431
irisge <[iris.genevrier@gmail.com](mailto:iris.genevrier@gmail.com)>
2023-03-02 19:41:51 +0100

    responsive 2xl

--- frontend/src/pages/Accueil.jsx
+++ frontend/src/pages/Accueil.jsx
@@ -15,22 +15,40 @@ function Accueil() {
   return (
     <div className="justify-center w-full grow grid grid-rows-mobile grid-cols-mobile md:grid-cols-5 lg:landscape:grid-cols-7 lg:landscape:grid-rows-desktop gradient-linear font-nunito text-white">
       <div className="col-start-2 lg:landscape:col-start-1 md:col-end-5 row-start-1 row-end-1 flex flex-col items-center justify-evenly">
-        <img className="lg:justify-self-start" src={logo} alt="logo" />
-        <h1 className="text-xl md:text-2xl lg:landscape:text-3xl xl:text-4xl">
+        <img
+          className="lg:justify-self-start 2xl:w-6/12"
+          src={logo}
+          alt="logo"
+        />
+        <h1 className="text-xl md:text-2xl lg:landscape:text-3xl xl:text-4xl 2xl:hidden">
           Garde d'enfant à la demande
         </h1>
       </div>
+      {/* <h1 className="max-2xl:hidden 2xl:flex 2xl:text-5xl col-start-5 col-end-8 row-start-2 row-end-2 w-full justify-self-center justify-center self-start">
+        Garde d'enfant à la demande
+      </h1> */}
       <img
-        className="col-span-3 md:col-start-2 lg:landscape:col-start-1 lg:landscape:col-end-5 row-start-2 row-end-2 lg:landscape:row-end-4 flex w-full lg:landscape:w-11/12 self-center justify-self-center lg:landscape:justify-self-start"
+        className="col-span-3 md:col-start-2 lg:landscape:col-start-1 lg:landscape:col-end-5 row-start-2 row-end-2 lg:landscape:row-end-4 flex w-full lg:landscape:w-10/12 self-center justify-self-center lg:landscape:justify-self-start"
         src={hero}
         alt="hero"
       />
 
-      <div className="col-start-2 md:col-start-2 md:col-span-3 lg:landscape:col-start-5 row-start-3 row-end-5 lg:landscape:row-start-2 lg:landscape:row-end-4 lg:landscape:self-end lg:landscape:justify-self-center flex flex-col justify-evenly lg:landscape:justify-betwwen w-full lg:landscape:landscape:w-3/4 lg:landscape:h-3/4">
-        <button type="button" className="btn-purple" onClick={handleConnect}>
+      <div className="col-start-2 md:col-start-2 md:col-span-3 lg:landscape:col-start-5 row-start-3 row-end-5 lg:landscape:row-start-2 lg:landscape:row-end-4 lg:landscape:self-end lg:landscape:justify-self-center flex flex-col justify-evenly lg:landscape:justify-betwwen w-full lg:landscape:landscape:w-3/4 2xl:landscape:w-full lg:landscape:h-3/4 2xl:landscape:items-center">
+        <h1 className="hidden 2xl:flex 2xl:text-5xl 3xl:text-6xl 2xl:w-full 2xl:justify-center ">
+          Garde d'enfant à la demande
+        </h1>
+        <button
+          type="button"
+          className="btn-purple 2xl:landscape:w-3/4"
+          onClick={handleConnect}
+        >
           Me connecter
         </button>
-        <button type="button" className="btn-orange" onClick={handleCreate}>
+        <button
+          type="button"
+          className="btn-orange 2xl:landscape:2xl:w-3/4"
+          onClick={handleCreate}
+        >
           Créer mon compte
         </button>
       </div>
--- frontend/src/pages/Connexion.jsx
+++ frontend/src/pages/Connexion.jsx
@@ -33,21 +33,26 @@ function Connexion() {
   };
 
   return (
-    <div className="gradient-linear grow grid grid-rows-connexion lg:grid-rows-none lg:grid-cols-3 max-lg:landscape:grid-rows-5 lg:landscape:grid-rows-none items-center justify-center h-full w-full font-nunito text-white">
+    <div className="justify-center w-full grow grid grid-rows-mobile grid-cols-mobile md:grid-cols-5 lg:landscape:grid-cols-7 lg:landscape:grid-rows-desktop gradient-linear font-nunito text-white">
       <img
+        className=" justify-self-center self-center col-start-1 col-end-4 md:col-start-2 lg:landscape:col-start-1 md:col-end-5 row-start-1 row-end-1 flex flex-col items-center justify-evenly"
         src={logo}
-        alt="logo NounouLink"
-        className="max-md:landscape:scale-50 max-lg:landscape:scale-75  justify-self-center  lg:self-start lg:p-8 row-start-1  lg:col-start-1 lg:col-span-1 "
+        alt="logo"
       />
+
       <img
+        className="col-span-3 md:col-start-2 lg:landscape:col-start-1 lg:landscape:col-end-5 row-start-2 row-end-2 lg:landscape:row-end-4 flex w-full lg:landscape:w-11/12 self-center justify-self-center lg:landscape:justify-self-start"
         src={hero}
-        alt="hero NounouLink"
-        className="hidden lg:flex max-lg:landscape:scale-50 lg:scale-[0.7] justify-self-center row-start-1 lg:self-end lg:col-start-1 lg:col-span-2"
+        alt="hero"
       />
-      <form className="flex flex-col items-center justify-evenly w-full h-full lg:h-[60%]  row-start-2 lg:row-start-auto max-lg:landscape:row-span-3">
-        <label className="mix-blend-screen" htmlFor="email">
+
+      <form className="col-start-1 md:col-start-2 col-span-3 lg:landscape:col-start-5 row-start-3 row-end-5 lg:landscape:row-start-2 lg:landscape:row-end-4 justify-self-center lg:landscape:self-end lg:landscape:justify-self-center flex flex-col items-center justify-evenly lg:landscape:justify-betwwen w-full lg:landscape:landscape:w-3/4 lg:landscape:h-3/4">
+        <label
+          className="mix-blend-screen flex flex-col w-10/12 h-full"
+          htmlFor="email"
+        >
           <input
-            className="bg-grey-input rounded-lg w-[300px] h-[60px] max-md:landscape:h-[40px] bg-opacity-30 placeholder-white pl-4"
+            className="input"
             type="email"
             name="email"
             id="email"
@@ -57,9 +62,12 @@ function Connexion() {
           />
           {error && <h2 className="text-red-500">{error}</h2>}
         </label>
-        <label className="mix-blend-screen" htmlFor="password">
+        <label
+          className="mix-blend-screen flex flex-col w-10/12 h-full"
+          htmlFor="password"
+        >
           <input
-            className="bg-grey-input rounded-lg w-[300px] h-[60px] max-md:landscape:h-[40px] bg-opacity-30 placeholder-white pl-4"
+            className="input"
             type="password"
             name="password"
             id="password"
@@ -79,13 +87,67 @@ function Connexion() {
           </button>
         </div>
       </form>
-      <div className="lg:hidden flex flex-row items-center justify-center gap-4">
+      {/* <div className="lg:hidden flex flex-row items-center justify-center gap-4">
         <p>Créer mon compte</p>
         <button type="submit" onClick={handleSubmit}>
           <img src={next} alt="go next" />
         </button>
-      </div>
+      </div> */}
     </div>
+
+    // // <div className="gradient-linear grow grid grid-rows-connexion lg:grid-rows-none lg:grid-cols-3 max-lg:landscape:grid-rows-5 lg:landscape:grid-rows-none items-center justify-center h-full w-full font-nunito text-white">
+    // //   <img
+    // //     src={logo}
+    // //     alt="logo NounouLink"
+    // //     className="max-md:landscape:scale-50 max-lg:landscape:scale-75  justify-self-center  lg:self-start lg:p-8 row-start-1  lg:col-start-1 lg:col-span-1 "
+    // //   />
+    // //   <img
+    // //     src={hero}
+    // //     alt="hero NounouLink"
+    // //     className="hidden lg:flex max-lg:landscape:scale-50 lg:scale-[0.7] justify-self-center row-start-1 lg:self-end lg:col-start-1 lg:col-span-2"
+    // //   />
+    //   // <form className="flex flex-col items-center justify-evenly w-full h-full lg:h-[60%]  row-start-2 lg:row-start-auto max-lg:landscape:row-span-3">
+    //   //   <label className="mix-blend-screen" htmlFor="email">
+    //       {/* <input
+    //         className="bg-grey-input rounded-lg w-[300px] h-[60px] max-md:landscape:h-[40px] bg-opacity-30 placeholder-white pl-4"
+    //         type="email"
+    //         name="email"
+    //         id="email"
+    //         placeholder="Adresse mail"
+    //         value={email}
+    //         onChange={handleEmailChange}
+    //       />
+    //       {error && <h2 className="text-red-500">{error}</h2>}
+    //     </label>
+    //     <label className="mix-blend-screen" htmlFor="password">
+    //       <input
+    //         className="bg-grey-input rounded-lg w-[300px] h-[60px] max-md:landscape:h-[40px] bg-opacity-30 placeholder-white pl-4"
+    //         type="password"
+    //         name="password"
+    //         id="password"
+    //         placeholder="Mot de passe"
+    //         value={password}
+    //         onChange={handlePasswordChange}
+    //       />
+    //     </label>
+    //     <button className="btn-purple" type="button">
+    //       Me connecter
+    //     </button>
+    //     <p>J'ai oublié mon mot de passe</p>
+    //     <div className="hidden lg:flex lg:flex-row lg:items-center lg:justify-center lg:gap-4 lg:mb-8">
+    //       <p>Créer mon compte</p>
+    //       <button type="submit" onClick={handleSubmit}>
+    //         <img src={next} alt="go next" />
+    //       </button>
+    //     </div>
+    //   </form> */}
+    // //   <div className="lg:hidden flex flex-row items-center justify-center gap-4">
+    // //     <p>Créer mon compte</p>
+    // //     <button type="submit" onClick={handleSubmit}>
+    // //       <img src={next} alt="go next" />
+    // //     </button>
+    // //   </div>
+    // // </div>
   );
 }
 
