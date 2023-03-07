557630e8b17be3ebf1e5b5599a4d6fd46a9ca430
Xcaliburns <davidabruzzogames@gmail.com>
2023-03-07 17:26:14 +0100

    matck ok

--- frontend/src/pages/SearchResults.jsx
+++ frontend/src/pages/SearchResults.jsx
@@ -1,8 +1,62 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import filter from "../assets/filter.svg";
 import trier from "../assets/trier.svg";
 
 function SearchResults() {
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}/api/creneaux`)
+      .then((response) => {
+        const matchSearch = JSON.parse(localStorage.getItem("matchSearch"));
+        // console.log(`matchsearch : ${matchSearch}`);
+        // console.log(`creneaux debut nanny: ${response.data[0].beginning_hour}`);
+        // console.log(`creneaux fin nanny: ${response.data[0].end_time}`);
+
+        // const beginNanny = Date.parse(response.data[0].beginning_hour);
+
+        // .getTimezoneOffset() c est ca qui deconne
+        // et si on tente getTime()
+        const beginNanny = new Date(
+          response.data[0].beginning_hour
+        ).toLocaleString("fr-FR", { timeZone: "Europe/Paris" });
+
+        const endNanny = new Date(response.data[0].end_time).toLocaleString(
+          "fr-FR",
+          { timeZone: "Europe/Paris" }
+        );
+
+        const beginParent = new Date(matchSearch[2]).toLocaleString("fr-FR", {
+          timeZone: "Europe/Paris",
+        });
+        const endParent = new Date(matchSearch[3]).toLocaleString("fr-FR", {
+          timeZone: "Europe/Paris",
+        });
+
+        // console.log(`beginNanny: ${beginNanny}`);
+        // console.log(`endNanny: ${endNanny}`);
+        // console.log(`beginParent: ${beginParent}`);
+        // console.log(`endParent: ${endParent}`);
+
+        // console.log(beginNanny, endNanny, beginParent, endParent);
+
+        if (beginNanny <= beginParent && endNanny >= endParent) {
+          alert("Youpi go faire un brunch");
+        } else {
+          alert("bon ben on commande un pizza");
+        }
+        // return response.data
+
+        // tu y arrives? oui mais il faut voir avec le decalage horaire
+        // t'es un génie
+        // c est toi le genie
+
+        // tu obtiens un GTM +2 pour la nounou ?
+        // je crois pas, enfin je sais pas , le time de la nounou est entre 6h et 10h au lieu de 8h 12h
+        // Avec new Date(response.blabla) j'arrive à obtenir 8h et 12h mais avec un GTM+2 ...
+      });
+  }, []);
+
   return (
     <>
       <label htmlFor="child">
