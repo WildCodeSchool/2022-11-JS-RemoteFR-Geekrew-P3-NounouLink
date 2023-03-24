fbe007b52c8f7a2a87c32cf8bd96feac8dfecf78
Xcaliburns <davidabruzzogames@gmail.com>
2023-03-24 13:57:02 +0100

    menu WIP

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
-import { useUserContext } from "../contexts/UserContext";
+
 import Trombone from "../assets/trombone.svg";
 import Loupe from "../assets/Loupe.svg";
 import EnfantAdd from "../assets/enfantAdd.svg";
@@ -9,7 +9,7 @@ import home from "../assets/home.svg";
 function Navbar() {
   const [blur, setBlur] = useState(false);
   const location = useLocation();
-  const { lastname, firstname } = useUserContext();
+
   const getActiveLinkStyle = (isActive) => {
     if (isActive) {
       return { filter: "blur(0px)" };
@@ -27,44 +27,47 @@ function Navbar() {
           to="/FormulaireEnfant"
         />
 
-        <NavLink
-          className="flex flex-row  justify-end text-white   text-left pr-5"
-          activeclassname="active"
-        >
-          <p className="text-2xl ml-10 pb-5 ">{`${lastname}  ${firstname}`}</p>
-        </NavLink>
+        <div className="mb-8 mt-8">
+          <div className="flex flex-row justify-evenly pb-2 pt-2 ">
+            <NavLink
+              style={getActiveLinkStyle(location.pathname === "/Menu")}
+              to="/menu"
+            >
+              <img alt="Menu principal" src={home} className="scale-150" />
+            </NavLink>
+            <NavLink
+              style={getActiveLinkStyle(
+                location.pathname === "/FormulaireParent"
+              )}
+              activeclassname="active"
+              to="/FormulaireParent"
+            >
+              <img alt="documents" src={Trombone} className="scale-150 " />
+            </NavLink>
 
-        <div className="flex flex-row justify-evenly pb-2 pt-2 ">
-          <NavLink
-            style={getActiveLinkStyle(location.pathname === "/Menu")}
-            to="/menu"
-          >
-            <img alt="Menu principal" src={home} />
-          </NavLink>
-          <NavLink
-            style={getActiveLinkStyle(
-              location.pathname === "/FormulaireParent"
-            )}
-            activeclassname="active"
-            to="/FormulaireParent"
-          >
-            <img alt="documents" src={Trombone} />
-          </NavLink>
-
-          <NavLink
-            style={getActiveLinkStyle(
-              location.pathname === "/FormulaireEnfant"
-            )}
-            to="/FormulaireEnfant"
-          >
-            <img alt="ajouter un enfant" src={EnfantAdd} />
-          </NavLink>
-          <NavLink
-            style={getActiveLinkStyle(location.pathname === "/Search")}
-            to="/recherche"
-          >
-            <img alt="rechercher une nounou" src={Loupe} />
-          </NavLink>
+            <NavLink
+              style={getActiveLinkStyle(
+                location.pathname === "/FormulaireEnfant"
+              )}
+              to="/FormulaireEnfant"
+            >
+              <img
+                alt="ajouter un enfant"
+                src={EnfantAdd}
+                className="scale-150"
+              />
+            </NavLink>
+            <NavLink
+              style={getActiveLinkStyle(location.pathname === "/Search")}
+              to="/recherche"
+            >
+              <img
+                alt="rechercher une nounou"
+                src={Loupe}
+                className="scale-150 "
+              />
+            </NavLink>
+          </div>
         </div>
       </nav>
     </div>
--- frontend/src/pages/Menu.jsx
+++ frontend/src/pages/Menu.jsx
@@ -1,25 +1,48 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import Navbar from "../components/Navbar";
-// import { useUserContext } from "../contexts/UserContext";
+import Trombone from "../assets/trombone.svg";
+import Loupe from "../assets/Loupe.svg";
+import EnfantAdd from "../assets/enfantAdd.svg";
+
+import logo from "../assets/logo.svg";
 
 function Menu() {
-  // const { firstname, lastname } = useUserContext();
-
   return (
-    <div className="flex flex-col h-full ">
-      <Navbar />
-
+    <div className=" gradient-linear flex flex-col h-full ">
       <div className="flex flex-col  items-center mt-20 ">
-        <nav className="flex btn-orange xl:col-start-2 xl:row-start-5 max-md:landscape:scale-75 max-lg:landscape:scale-75 mt-10 text-center text-base items-center justify-center ">
-          <NavLink to="/formulaireparent">Mettre à jour mes documents</NavLink>
-        </nav>
-        <nav className="flex btn-orange xl:col-start-2 xl:row-start-5 max-md:landscape:scale-75 max-lg:landscape:scale-75 mt-10 text-center text-base items-center justify-center">
-          <NavLink to="/formulaireenfant">ajouter un enfant</NavLink>
-        </nav>
-        <nav className=" flex btn-orange xl:col-start-2 xl:row-start-5 max-md:landscape:scale-75 max-lg:landscape:scale-75 mt-10 text-center text-base items-center justify-center">
-          <NavLink to="/recherche">rechercher une nounou</NavLink>
-        </nav>
+        <img
+          src={logo}
+          alt="logo NounouLink"
+          className="max-md:landscape:scale-50 max-lg:landscape:scale-75  justify-self-center   lg:p-8 row-start-1  lg:col-start-1 lg:col-span-1 "
+        />
+        <div className="flex flex-row align-center">
+          <nav className="flex btn-orange xl:col-start-2 xl:row-start-5 max-md:landscape:scale-75 max-lg:landscape:scale-75 mt-10 text-center text-base items-center justify-center ">
+            <img alt="documents" src={Trombone} className=" scale-150 mr-14" />{" "}
+            <NavLink to="/formulaireparent">Mes documents</NavLink>
+          </nav>
+        </div>
+
+        <div className="flex flex-row align-center">
+          <nav className="flex btn-orange xl:col-start-2 xl:row-start-5 max-md:landscape:scale-75 max-lg:landscape:scale-75 mt-10 text-center text-base items-center justify-center">
+            <img
+              alt="ajouter un enfant"
+              src={EnfantAdd}
+              className=" scale-150 mr-12 "
+            />
+            <NavLink to="/formulaireenfant">ajouter un enfant</NavLink>
+          </nav>
+        </div>
+
+        <div className="flex flex-row align-center">
+          <nav className=" flex btn-orange xl:col-start-2 xl:row-start-5 max-md:landscape:scale-75 max-lg:landscape:scale-75 mt-10 text-center text-base items-center justify-center">
+            <img
+              alt="rechercher une nounou"
+              src={Loupe}
+              className=" scale-150 mr-1 "
+            />
+            <NavLink to="/recherche">rechercher une nounou</NavLink>
+          </nav>
+        </div>
       </div>
     </div>
   );
